e8021eb6924e76dff4d2ab1057b8787e3abed23f
Walisson Portela <walissonportela36@gmail.com>
2022-12-11 16:23:13 -0300

    Tarefa prática - Publish/Subscribe

--- services/report/app.js
+++ services/report/app.js
@@ -24,7 +24,9 @@ async function printReport() {
 }
 
 async function consume() {
-    //TODO: Constuir a comunicação com a fila 
+    //TODO: Constuir a comunicação com a fila
+    console.log(`INSCRITO COM SUCESSO NA FILA: ${process.env.RABBITMQ_QUEUE_NAME}`)
+    await (await RabbitMQService.getInstance()).consume(process.env.RABBITMQ_QUEUE_NAME, (msg) => {processMessage(msg)}) 
 } 
 
 consume()
